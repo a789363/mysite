973adcfb1c372916948090271261007ddd268faf
a789363 <bike851013@gmail.com>
2021-03-23 17:28:35 +0800

    first commit

---
+++ 0323.php
@@ -0,0 +1,9 @@
+<?php
+ $chi = 90;
+ $en  = 95;
+ $math = 72;
+ $his = 85;
+ $total = $chi + $en + $math + $his;
+ $avg = $total /4 ;
+ echo "Total is $total.Average is $avg";
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>林育葦的練習網頁</title>
+	<style>
+	
+
+			p {
+			  font-family: 微軟正黑體;
+			  font-size: 20px;
+		}
+	</style>
+</head>
+<body>
+
+<h1>歡迎光臨我的網站</h1>
+<button><a href="/mysite2021">Home</a></button>
+<button><a href="https://ic.nkust.edu.tw/">>智慧商務系</a></button>
+<button><a href="http://tzukuan.nkust.org">計畫網站</a></button>
+<select onchange='location.href ="/mysite2021/"+this.value'>
+	<option value="0323-1.php">選擇要選的練習</option>
+	<option value="0323-1.php">0323-1</option>
+	<option value="0323-1.php">0323-2</option>
+</select>
+<hr>
+
+
+<p>有什麼問題歡迎連絡 C109156118@nkust.edu.tw</p>
+
+</body>
+</html>
